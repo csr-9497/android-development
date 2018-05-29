f1b17b1d87d63701f586c46859facb1a1bd790e8
csr9497 <cesaraop.12@gmail.com>
2018-05-28 23:29:22 -0500

    start

---
+++ app/src/main/java/app/desarrollo/proyecto/plasiette/MainActivity.java
@@ -0,0 +1,13 @@
+package app.desarrollo.proyecto.plasiette;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_sign_up);
+    }
+}
