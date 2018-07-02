42aeff02ed268aaa6911f23e2ceb417cb9110779
csr-9497 <cesaraop.12@gmail.com>
2018-07-02 02:20:36 -0500

    .

--- app/src/main/java/app/desarrollo/proyecto/plasiette/Menu.java
+++ app/src/main/java/app/desarrollo/proyecto/plasiette/Menu.java
@@ -11,9 +11,10 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.Toolbar;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
@@ -24,12 +25,12 @@ import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-
 public class Menu extends AppCompatActivity {
 
-    String[] images;
-    String[] comidas;
-    String[] precios;
+    String[] images=new String[5];
+    String[] comidas= new String[5];
+    String[] precios=new String[5];
+
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -38,17 +39,12 @@ public class Menu extends AppCompatActivity {
         String ruc="11111111111";
         MenuTask menuTask = new MenuTask();
         menuTask.execute(url,ruc);
-
-       // ListView listView = (ListView) findViewById(android.R.id.list);
-
-        //CustomAdapter customAdapter= new CustomAdapter();
-       // listView.setAdapter(customAdapter);
     }
 
     class CustomAdapter extends BaseAdapter{
         @Override
         public int getCount() {
-            return 5;
+            return comidas.length;
         }
 
         @Override
@@ -66,20 +62,18 @@ public class Menu extends AppCompatActivity {
             view = getLayoutInflater().inflate(R.layout.menu_row, null);
             ImageView imageView= (ImageView) view.findViewById(R.id.imagePlate);
             TextView textView= (TextView) view.findViewById(R.id.comida_title);
+            TextView textView2= (TextView) view.findViewById(R.id.price);
 
             imageView.setImageResource(R.drawable.comida1);
             textView.setText(comidas[i]);
-
+            textView2.setText(precios[i]);
             return view;
         }
     }
     private class MenuTask extends AsyncTask<String, Void, Void> {
-        String platos[]=new String[5];
-        private static final String TAG = "";
-
+        private static final String TAG = "Log TAG: ";
         @Override
         protected Void doInBackground(String... strings) {
-
             String result = "";
             try {
                 URL url = new URL(strings[0]);
@@ -98,33 +92,37 @@ public class Menu extends AppCompatActivity {
                 writer.close();
                 os.close();
                 con.connect();
-
                 BufferedReader bf = new BufferedReader(new InputStreamReader(con.getInputStream()));
                 result = bf.readLine();
-
 
                 JSONObject jsonObject = new JSONObject(result);
                 JSONArray jsonArray = jsonObject.getJSONArray("rows");
                 int lengthJsonArr = jsonArray.length();
                 for (int i = 0; i < lengthJsonArr; i++) {
-                    JSONObject jsonChildNode = jsonArray.getJSONObject(i);
-                       Log.i("Log_tag",jsonChildNode.getString("details"));
-                      //precios[i] = jsonChildNode.getString("price");
-                   //   images[i]= jsonChildNode.getString("image");
+                    try {
+                        JSONObject jsonChildNode = jsonArray.getJSONObject(i);
+                        comidas[i] = jsonChildNode.getString("details");
+                        precios[i] = jsonChildNode.getString("price");
+                        images[i]=jsonChildNode.getString("image");
+                    }
+                    catch (JSONException e){
+                        e.printStackTrace();
+                    }
                 }
                 //jsonObject.getBoolean("auth");
-
             } catch (Exception e) {
                 e.printStackTrace();
                 Log.d(TAG, ""+e);
             }
             return null;
         }
-
         @Override
         protected void onPostExecute(Void aVoid) {
             super.onPostExecute(aVoid);
+
+            ListView listView = (ListView) findViewById(android.R.id.list);
+            CustomAdapter customAdapter= new CustomAdapter();
+            listView.setAdapter(customAdapter);
         }
     }
-
 }
